475e27a976e7f5632eac51deb020de2ee69e455b
simranpreet kaur <simranaug24@gmail.com>
2021-01-24 22:04:23 +0530

    Initial Commit

---
+++ animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animation
+//
+//  Created by simranPreet KAur on 24/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
